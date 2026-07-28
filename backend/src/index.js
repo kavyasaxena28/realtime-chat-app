91028c42815b18e689bd24726d3feca6a30eae56
Kavya Saxena <kavyasaxena2811@gmail.com>
2026-07-28 13:04:14 +0530

    Fix backend index

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,6 +40,39 @@
 //   console.log("server is running on PORT:" + PORT);
 //   connectDB();
 // });
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
+});
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
